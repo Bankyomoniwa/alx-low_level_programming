ad7a61bc4c731930d08808fce5a18f62ef17cd42
Banky Omoniwa <bankoleomoniwa@gmail.com>
2022-10-19 07:53:07 -0700

    initial commit

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,9 +5,10 @@
  *  _putchar -  writes the charcatec to standout
  *  @c: the character to print
  *
- *  Return:On sucess 1
+ *  Return: On sucess 1
  */
 int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
+
