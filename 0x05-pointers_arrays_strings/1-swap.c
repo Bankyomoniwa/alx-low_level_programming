15777016b66dba512e1457585ea9f5548092aec4
Banky Omoniwa <bankoleomoniwa@gmail.com>
2022-10-25 06:07:48 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of the two integers
+ * @a: int a
+ * @b: int b
+ */
+void swap_int(int *a, int *b)
+{
+	int q;
+
+	q = *a;
+	q = *b;
+	*b = q;
+}
+
